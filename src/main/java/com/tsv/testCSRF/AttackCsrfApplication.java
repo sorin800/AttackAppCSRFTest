69c34abe664f6b2930f5b68807b29f3ef855976f
Sorin <sorintruica@gmail.com>
2018-12-31 14:25:30 +0200

    commit

---
+++ src/main/java/com/tsv/testCSRF/AttackCsrfApplication.java
@@ -0,0 +1,14 @@
+package com.tsv.testCSRF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttackCsrfApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tsv/testCSRF/AttackCsrfApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tsv.testCSRF;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AttackCsrfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
